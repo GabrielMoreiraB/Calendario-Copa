14f87616fb21dbf23a214d2e0ef5eac42e25e034
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2022-11-03 17:38:03 -0300

    add animações

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ function createGame (player1, hour, player2) {
     `
 }
 
-
+let delay = -0.5;
 function createCard (date, day, games) {
+    delay = delay +0.5;
     return `
-    <div class="card">
+    <div class="card" style="animation-delay:${delay}s">
             <h2>${date} <span>${day}</span></h2>
             <ul>
                 ${games}   
@@ -21,16 +22,7 @@ function createCard (date, day, games) {
 }
 
 
-document.querySelector("#app").innerHTML=`
-    <header>
-        <img src="./assests/logo.svg" alt="logo">
-    </header>
-    <main id="cards">
-      ${createCard("24/11", "quinta",
-      createGame("brazil", "16:00", "serbia"))} 
-      ${createCard("24/11", "quinta",
-      createGame("brazil", "13:00", "switzerland")+ createGame("portugal", "16:00", "uruguay"))} 
-      ${createCard("24/11", "quinta",
-      createGame("brazil", "16:00", "cameroon"))} 
-    </main>
-`
+document.querySelector("#cards").innerHTML=    
+      createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+      createCard("24/11", "quinta", createGame("brazil", "13:00", "switzerland")+ createGame("portugal", "16:00", "uruguay")) +
+      createCard("24/11", "quinta", createGame("brazil", "16:00", "cameroon"))
